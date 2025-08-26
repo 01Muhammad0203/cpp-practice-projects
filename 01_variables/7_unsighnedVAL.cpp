122321c732b9da2a6d01b418dd1ef516e3cd5a63
01Muhammad0203 <164690288+01Muhammad0203@users.noreply.github.com>
2025-08-27 01:10:40 +0300

    Create 7_unsighnedVAL.cpp
    Беззнаковая переменная
    Создать unsigned int, присвоить отрицательное число и вывести, что получится.

---
+++ 01_variables/7_unsighnedVAL.cpp
@@ -0,0 +1,14 @@
+using std::cout;
+using std::cin;
+using std::string;
+using std::endl;
+
+int main(){
+
+    unsigned int a = -100; // unsigned_value = 2^32 + (-100) = 4294967296 - 100 = 4294967196
+  
+    cout << a;
+
+    return 0;
+}
+// Вывод: 4294967196 
