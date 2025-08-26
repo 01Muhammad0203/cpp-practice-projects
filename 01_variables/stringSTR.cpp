56ad20a498b1a02c137ae623277ef42d523ea4ed
01Muhammad0203 <164690288+01Muhammad0203@users.noreply.github.com>
2025-08-27 01:02:57 +0300

    Create stringSTR.cpp
    Склейка строк
    Создать 2 строки(std::string) и объединить их в одну.

---
+++ 01_variables/stringSTR.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+using std::cout;
+using std::string;
+
+int main() {
+    string a = "I will be ";
+    string b = "  senior C++ developer, soon";
+    cout << a + b;
+    return 0;
+}
