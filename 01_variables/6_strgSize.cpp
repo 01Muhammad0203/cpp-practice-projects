e67fdf431443614a164cb54058e9cc8aa4692a5c
01Muhammad0203 <164690288+01Muhammad0203@users.noreply.github.com>
2025-08-27 01:07:15 +0300

    Create 6_strgSize.cpp
    Счётчик памяти
    Вывести размер(sizeof) всех базовых типов(int, short, long long, float, double, char, string).

---
+++ 01_variables/6_strgSize.cpp
@@ -0,0 +1,10 @@
+using std::cout;
+using std::cin;
+using std::string;
+using std::endl;
+int main() {
+   
+    cout << "int = " << sizeof(int) << endl << "short = " << sizeof(short) << endl << "long long = " << sizeof(long long) << endl << "float = " << sizeof(float) << endl << "double" << sizeof(double) << endl << "char = " << sizeof(char) << endl << "string = " << sizeof(char) << endl;
+
+    return 0;
+}
