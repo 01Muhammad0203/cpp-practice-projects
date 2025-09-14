a7da7bf1a206ba977d275655bdd1ba15ff9d5910
01Muhammad0203 <164690288+01Muhammad0203@users.noreply.github.com>
2025-09-15 00:14:05 +0300

    Create 12. DoubleCONST.cpp
    Сделай константу const double PI = 3.14159; и выведи её на экран.

---
+++ 01_variables/12. DoubleCONST.cpp
@@ -0,0 +1,12 @@
+/*
+Сделай константу const double PI = 3.14159; и выведи её на экран.
+*/
+#include <iostream>
+
+
+int main() {
+	const double PI = 3.14159;
+	std::cout << "Pi is " << PI << " !\n";
+
+
+}
