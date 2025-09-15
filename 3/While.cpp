b85f75e08b611a6d98f02aaf0d2918aaf7ae3008
01Muhammad0203 <164690288+01Muhammad0203@users.noreply.github.com>
2025-09-15 16:18:03 +0300

    Create While.cpp
    Программа - Обратный отсчет

---
+++ 3/While.cpp
@@ -0,0 +1,20 @@
+/*Программа - Обратный отсчет*/
+#include <iostream>
+#include <string>
+using std::string;
+using std::cout;
+
+
+
+int main() {
+	int n = 10;
+	while (n > 0) {
+		cout << n << "...\n";
+		n--;
+	}
+
+	std::cout << "Start!\n";
+
+	return 0;
+
+}
